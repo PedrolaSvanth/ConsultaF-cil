1f18f739bbef1fe39d842a6fcd1cd074c153c95e
PedrolaSvanth <pedro1pepecraft@gmail.com>
2025-10-23 17:06:05 -0300

    FINALIZADO

--- controllers/login.php
+++ controllers/login.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $_SESSION['usuario_id'] = $usuario['id'];
             $_SESSION['usuario_nome'] = $usuario['nome'];
             
-            header("Location: ../pages/dashboard.html");
+            header("Location: ../assets/img/EM_CONSTRUCAO.jpg");
             exit;
 
         } else {
